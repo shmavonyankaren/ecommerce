675d1f62ba2dcbb369d664a250551d797a7e4084
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-12-09 10:39:02 +0400

    create react app and install all necessary dedevDependencies and dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+const App = () => {
+
+
+    return (
+        <div className="">
+
+        </div>  
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./index.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const containerRoot = document.getElementById("root");
+
+const root = ReactDOM.createRoot(containerRoot);
+
+root.render(<App />);
