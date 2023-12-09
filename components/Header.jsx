c64c805b5c46b7475e7520ba23e1fa2aa1a48fcd
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-12-09 10:48:14 +0400

    create header section and add routes for different pages

---
+++ components/Header.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { AiOutlineShopping } from "react-icons/ai";
+import { NavLink } from 'react-router-dom';
+
+const navigation = [
+    { name: 'Products', href: '/products'},
+    { name: "Cart", href: '/cart'},
+    { name: "Contact", href: "/contact"},
+];
+
+export default function Header({quatities, children}) {
+
+    return (
+        <div className='bg-gray-300 min-h-screen'>
+            <header className="bg-white fixed w-[100%] opacity-[0.97] hover:bg-gray-800 hover:opacity-100">
+                <nav  className="bg-gray-800">
+                    <>
+                        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                            <div className="relative flex h-16 items-center justify-between">
+                                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                    <div className="flex flex-shrink-0 items-center">
+                                        <div className="flex items-center mr-2">
+                                            <h1 className='text-white  font-semibold'>Ecommerce</h1>
+                                        </div>
+                                        <img
+                                            className="block h-8 w-auto lg:hidden"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                            alt="Ecommerce"
+                                        />
+                                        <img
+                                            className="hidden h-8 w-auto lg:block"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                            alt="Ecommerce"
+                                        />
+                                    </div>
+                                    <div className="hidden sm:ml-6 sm:block mt-[10px]">
+                                        <div className="flex space-x-4">
+                                            {navigation.map((item) => (
+                                                <NavLink
+                                                    key={item.name}
+                                                    to={item.href}
+                                                    className={({isActive}) =>{
+                                                    return( 'no-underline rounded-md px-3 py-2 text-sm font-medium' + (
+                                                        isActive
+                                                        ? ' bg-gray-900 text-white' 
+                                                        : ' text-gray-300 hover:bg-gray-700 hover:text-white'
+                                                    ))
+                                                    }}
+                                                    aria-current={item.current ? 'page' : undefined}
+                                                >
+                                                    {item.name}
+                                                </NavLink>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                    <NavLink
+                                        type='button' 
+                                        className='ml-5 text-2xl text-gray-300 cursor-pointer relative transition transform duration-75 ease-in-out hover:scale-150'
+                                        to='/cart'
+                                    >
+                                        <AiOutlineShopping />
+                                        <span className='flex justify-center items-center absolute left-[12px] top-0 text-[12px] text-white bg-red-600 w-[18px] h-[18px] rounded-[50%] font-semibold'>
+                                            {quatities}
+                                        </span>
+                                    </NavLink>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="sm:hidden">
+                            <div className="space-y-1 px-2 pb-3 pt-2">
+                                {navigation.map((item) => (
+                                    <NavLink
+                                                key={item.name}
+                                                to={item.href}
+                                                className={({isActive}) =>{
+                                                return( 'no-underline rounded-md px-3 py-2 text-sm font-medium' + (
+                                                    isActive
+                                                    ? ' bg-gray-900 text-white' 
+                                                    : ' text-gray-300 hover:bg-gray-700 hover:text-white'
+                                                ))
+                                                }}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                    </NavLink>
+                                ))}
+                            </div>
+                        </div>
+                    </>
+                </nav>
+            </header>
+            <div className='pt-16'>
+                {
+                    children
+                }
+            </div>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import Header from "./components/Header";
+import { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+export const totalQuantities = createContext(0);
 
 const App = () => {
-
+    let [quatities, setQuantities] = useState(0);
 
     return (
         <div className="">
-
+            <BrowserRouter>
+                <Header quatities={quatities} >
+                    <Routes>
+                        <Route path="/*" element={<Main addToTheCart={addToTheCart} changeQuantities={changeQuantities}/>} />
+                        <Route path="/products" element={<Main addToTheCart={addToTheCart} changeQuantities={changeQuantities}/>} />
+                        <Route path="/cart" element={<Cart setQuantities={setQuantities} changeQuantities={changeQuantities} products={products} totalPrice={totalPrice} setProducts={setProducts}/>} />
+                        <Route path="/success" element={<Success setProducts={setProducts} setQuantities={setQuantities} />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/successSent" element={<SuccessSent />} />
+                    </Routes>    
+                </Header>
+            </BrowserRouter>
         </div>  
     )
 }
